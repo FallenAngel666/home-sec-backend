1b8b77d28b396025bfb952a427991b32d793b4de
luziferus <luzy-dev@pm.me>
2024-01-08 20:51:11 +0100

    feat: add status service

---
+++ src/status/service.rs
@@ -0,0 +1,54 @@
+use uuid::Uuid;
+use crate::common::database::DBResponse;
+use crate::status::repository;
+use crate::status::repository::Status;
+
+pub enum ServiceResponse<T> {
+    Ok(T),
+    NotFound,
+    Error(String)
+}
+
+pub fn log_status(status: &Status) -> ServiceResponse<Status> {
+    match repository::save_status(status) {
+        DBResponse::Ok(id) => {
+            match repository::get_status(&id) {
+                DBResponse::Ok(s) => { ServiceResponse::Ok(s)}
+                DBResponse::NotFound => { ServiceResponse::Error("Could not find created status entity.".to_string())}
+                DBResponse::Error(err) => { ServiceResponse::Error(err)}
+            }
+        }
+        DBResponse::Error(err) => { ServiceResponse::Error(err)}
+        _ => ServiceResponse::Error("Something unexpected happened.".to_string())
+    }
+}
+
+pub fn get_status_by_id(id: &Uuid) -> ServiceResponse<Status> {
+    match repository::get_status(id) {
+        DBResponse::Ok(s) => { ServiceResponse::Ok(s)}
+        DBResponse::NotFound => { ServiceResponse::NotFound}
+        DBResponse::Error(err) => { ServiceResponse::Error(err)}
+    }
+}
+
+pub fn get_all_statuses() -> ServiceResponse<Vec<Status>> {
+    match repository::get_all_statuses() {
+        DBResponse::Ok(s) => { ServiceResponse::Ok(s)}
+        DBResponse::Error(err) => { ServiceResponse::Error(err)}
+        _ => ServiceResponse::Error("Something unexpected happened.".to_string())
+    }
+}
+
+pub fn delete_status_by_id(id: &Uuid) -> ServiceResponse<Status> {
+    let exiting_status = match get_status_by_id(id) {
+        ServiceResponse::Ok(s) => {s}
+        ServiceResponse::NotFound => {return ServiceResponse::NotFound}
+        ServiceResponse::Error(err) => {return ServiceResponse::Error(err)}
+    };
+
+    match repository::delete_status(id) {
+        DBResponse::Ok(_) => {ServiceResponse::Ok(exiting_status)}
+        DBResponse::NotFound => {ServiceResponse::NotFound}
+        DBResponse::Error(err) => {ServiceResponse::Error(err)}
+    }
+}
